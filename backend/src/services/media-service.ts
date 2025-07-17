6789dac63ea035f0f7de8bf398e9f6cd9511abbd
Alec <alec_z17@web.de>
2025-07-18 00:36:55 +0200

    added base for media service

--- backend/src/services/media-service.ts
+++ backend/src/services/media-service.ts
@@ -5,6 +5,14 @@ import ffmpeg from 'fluent-ffmpeg';
 import { TRANSCODE_PATH } from "../app";
 import { getCombinedMetadata } from "../utils/media-utils";
 
-const sessions = new Map();
 const SEGMENT_DURATION = 10; // seconds per segment
 const BUFFER_SEGMENTS = 3; // x times segment_duration for extra buffer
+
+class MediaService {
+    private sessions: Map<string, any> = new Map();
+}
+
+const mediaService = new MediaService();
+
+export default mediaService;
+export { MediaService };
