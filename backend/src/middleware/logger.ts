4eeea3945be285fef5ed3a4fef27e5f7b2b763ed
Alec <alec_z17@web.de>
2025-07-22 19:46:33 +0200

    adjust comments, added connection close handling

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -14,24 +14,31 @@ export function loggerHandler(req: Request, res: Response, next: NextFunction): 
     const traceId = randomUUID();
     const start = performance.now()
     const { method, originalUrl } = req;
-    const { statusCode } = res;
 
     req.traceId = traceId;
 
     res.on('finish', () => {
         const duration = performance.now() - start;
+        const { statusCode } = res;
         const statusColor = getStatusColor(statusCode);
 
-        console.log(`[${traceId}] - ${method} - ${originalUrl} - ${statusColor}${statusCode}\x1b[0m - ${duration.toFixed(2)}ms`);
+        console.log(`[${traceId}] -> ${method} - ${originalUrl} - ${statusColor}${statusCode}\x1b[0m - ${duration.toFixed(2)}ms`);
+    });
+
+    res.on('close', () => {
+        if (!res.headersSent) {
+            const duration = performance.now() - start;
+            console.log(`[${traceId}] ! ${method} ${originalUrl} \x1b[31mCONNECTION_CLOSED\x1b[0m - ${duration.toFixed(2)}ms`);
+        }
     });
 
     next();
 }
 
 function getStatusColor(statusCode: number): string {
-    if (statusCode >= 500) return '\x1b[31m'; // Rot für 5xx
-    if (statusCode >= 400) return '\x1b[33m'; // Gelb für 4xx
-    if (statusCode >= 300) return '\x1b[36m'; // Cyan für 3xx
-    if (statusCode >= 200) return '\x1b[32m'; // Grün für 2xx
-    return '\x1b[37m'; // Weiß für andere
+    if (statusCode >= 500) return '\x1b[31m'; // red for 5xx
+    if (statusCode >= 400) return '\x1b[33m'; // yellow for 4xx
+    if (statusCode >= 300) return '\x1b[36m'; // cyan for 3xx
+    if (statusCode >= 200) return '\x1b[32m'; // green for 2xx
+    return '\x1b[37m'; // white for others
 }
