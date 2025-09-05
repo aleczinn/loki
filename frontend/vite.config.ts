ffccc022fb48a68734f324f30904b0870a797a1a
Alec <alec_z17@web.de>
2025-09-06 00:06:45 +0200

    moved to tailwind v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, loadEnv, type ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default ({ mode }: ConfigEnv) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return defineConfig({
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            tailwindcss()
+        ],
         server: {
             host: '0.0.0.0',
             port: 5173,
