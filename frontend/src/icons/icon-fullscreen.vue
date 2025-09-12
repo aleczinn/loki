a2dd515eb17ffd7cb18c63c94511deabccce732c
Alec <alec_z17@web.de>
2025-09-12 15:06:59 +0200

    added fullscreen icon

---
+++ frontend/src/icons/icon-fullscreen.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_2_65436)">
+            <path d="M14.25 2.25V0.75H23.25V9.75H21.75V3.31064L14.2178 10.8428L13.1572 9.7822L20.6894 2.25H14.25Z" fill="currentColor"/>
+            <path d="M2.24998 14.2495V20.6889L9.96968 12.9692L11.0303 14.0298L3.31062 21.7495H9.74998V23.2495H0.749977V14.2495H2.24998Z" fill="currentColor"/>
+        </g>
+        <defs>
+            <clipPath id="clip0_2_65436">
+                <rect width="24" height="24" fill="white" transform="matrix(-1 0 0 1 24 0)"/>
+            </clipPath>
+        </defs>
+    </svg>
+
+</template>
