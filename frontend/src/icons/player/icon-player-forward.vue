383de5d13d3605217b026bfcd103f7fbbb00092a
Alec <alec_z17@web.de>
2025-09-12 11:40:19 +0200

    added/adjust icons svg's

---
+++ frontend/src/icons/player/icon-player-forward.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13 6V18L21.5 12L13 6ZM4 18L12.5 12L4 6V18Z" fill="currentColor"/>
+    </svg>
+</template>
